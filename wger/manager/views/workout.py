e11708a19876468d0e2357bb73024f98b4a482d8
Paul Upendo <Upendo@Pauls-MacBook-Pro.local>
2017-05-04 14:04:32 +0300

    Add export-workout option on UI. Implement feature functionality

--- wger/manager/views/workout.py
+++ wger/manager/views/workout.py
@@ -19,13 +19,17 @@ import uuid
 import datetime
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
 from django.template.context_processors import csrf
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.utils.translation import ugettext_lazy, ugettext as _
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import DeleteView, UpdateView
+from django.core import serializers
+
+
+import json
 
 from wger.core.models import (
     RepetitionUnit,
@@ -41,13 +45,16 @@ from wger.manager.models import (
 from wger.manager.forms import (
     WorkoutForm,
     WorkoutSessionHiddenFieldsForm,
-    WorkoutCopyForm
+    WorkoutCopyForm,
+    WorkoutExportForm
 )
 from wger.utils.generic_views import (
     WgerFormMixin,
     WgerDeleteMixin
 )
 from wger.utils.helpers import make_token
+
+from wger.manager.api import resources
 
 
 logger = logging.getLogger(__name__)
@@ -121,9 +128,37 @@ def view(request, pk):
     return render(request, 'workout/view.html', template_data)
 
 
-@login_required
-def export_workout():
-    pass
+def export(request, pk):
+    workout = get_object_or_404(Workout, pk=pk)
+    user = workout.user
+    is_owner = request.user == user
+
+    if not is_owner and not user.userprofile.ro_access:
+        return HttpResponseForbidden()
+   
+    # Process request
+    if request.method == 'POST':
+        workout_export_form = WorkoutExportForm(request.POST)
+
+        data = serializers.serialize('json', Workout.objects.filter(user=request.user))
+        response = HttpResponse(data, content_type='application/force-download')
+        response['Content-Disposition'] = 'attachment; filename="workouts.json"'  
+
+        return response
+    
+    else:
+        workout_export_form = WorkoutExportForm({'comment': workout.comment})
+
+        template_data = {}
+        template_data.update(csrf(request))
+        template_data['title'] = _('Export Workout')
+        template_data['form'] = workout_export_form
+        template_data['form_action'] = reverse('manager:workout:export', kwargs={'pk': workout.id})
+        template_data['form_fields'] = [workout_export_form['comment']]
+        template_data['submit_text'] = _('Export')
+        template_data['extend_template'] = 'base_empty.html' if request.is_ajax() else 'base.html'
+
+        return render(request, 'form2.html', template_data)
 
 
 @login_required
