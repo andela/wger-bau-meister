69e7246f2a812d5915141d5eb9cb2cf434ec37f0
TinaMorale <tina.murimi@andela.com>
2017-05-17 16:14:06 +0300

    Muscle cache

--- wger/exercises/signals.py
+++ wger/exercises/signals.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 
-
+from django.core.cache import cache
 from django.db.models.signals import pre_save
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
